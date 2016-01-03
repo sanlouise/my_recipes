746f2d37aa71b853f8a275990f652f4e6e35441b
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-03 19:18:46 +0000

    Added recipe routes and add styles

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,8 @@
+class RecipesController < ApplicationController
+  
+  def index
+    @recipes = Recipe.all
+  end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  
+# Home routes  
   root 'pages#home'
   get '/home', to: 'pages#home' 
+  
+# Recipe routes
+  # get '/recipes', to: 'recipes#index' 
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # post '/recipes', to: 'recipes#create' 
+  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  # patch '/recipes/:id', to: 'recipes#update' 
+  # get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+  # delete '/recipes/:id', to: 'recipes#destroy'
+  
+   resources :recipes
 end
