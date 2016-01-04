63cddaa02af078038b85c4a31166ded3bffd38e5
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-04 11:44:11 +0000

    Create page to edit recipes, edit css, fix link navbar

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,6 +25,22 @@ class RecipesController < ApplicationController
     end
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated succesfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+    
+  end
+  
+  
   private
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description)
