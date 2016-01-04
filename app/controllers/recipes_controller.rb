344b4738ec120eb67bc65d916f381015dc32917f
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-04 11:14:43 +0000

    Handle actions for recipe form submission

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,5 +8,29 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:success] = "Your recipe was created succesfully!"
+      redirect_to recipes_path
+      
+    else
+      render :new
+    end
+  end
+  
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end 
+
+
+  
 
 end
