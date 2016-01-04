e81d05aab79a26ecdfc03e76a7e91e8355dfdc54
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-04 14:03:50 +0000

    Add uploader to new recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103154845) do
+ActiveRecord::Schema.define(version: 20160104130410) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160103154845) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
