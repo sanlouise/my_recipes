08aaba6580cafb142f69bd2d02f2a58edd0bdeb4
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-05 17:58:15 +0000

    Add styles to index and pagination function

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,7 @@ class Recipe < ActiveRecord::Base
   validates :description, presence: true, length: {minimum: 20, maximum: 500}
   mount_uploader :picture, PictureUploader
   validate :picture
+  default_scope -> { order(updated_at: :desc) }
   
   private
     def picture_size
