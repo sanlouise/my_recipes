1966a25dbbdace2bf692ad603dbdba68bb3da451
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-06 10:58:05 +0000

    Create chef show and index pages

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,9 @@
 class ChefsController < ApplicationController
   
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
+  end
+  
   def new 
     @chef = Chef.new
   end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,6 @@
 class Chef < ActiveRecord::Base
   has_many :recipes
-  before_save { self.name = name.capitalize }
+  before_save { self.name = name }
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { minimum: 2, maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
